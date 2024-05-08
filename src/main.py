73cfb0f7251ac233d0128cf4883525aebd262093
luna1474 <peulling@gmail.com>
2024-05-08 22:18:38 +0900

    main code update

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from webserver import webserver
 from socketlib import socketlib
 from ocr import ocr
 from Msg import Msg
+from mouselib import mouselib
 
 import time
 import queue
@@ -16,9 +17,11 @@ mouse_queue = queue.Queue()
 ocr_model = ocr()
 webserver_model = webserver(main_queue, webserver_queue)
 socket_model = socketlib("client", "172.30.101.158", 5050, main_queue, socket_queue)
+mouse_model = mouselib("client", "172.30.101.158", 5051, None, mouse_queue)
 
 webserver_model.start()
 socket_model.start()
+mouse_model.start()
 
 while(True):
     if not main_queue.empty():
@@ -36,21 +39,24 @@ while(True):
             with webserver_queue.mutex:
                 webserver_queue.queue.clear()
             webserver_queue.put(name_list)
-            
+
+            msg = Msg()
+            msg.msg = "ready"
+            socket_queue.put(msg)
         elif msg.msg == "mouse":
             try:
                 obj = ocr_results[msg.idx]
                 print(obj)
                 msg = Msg()
-                msg.msg = "mouse"
+                msg.msg = "mouse xy"
                 msg.x = obj[1]
                 msg.y = obj[2]
-                socket_queue.put(msg)
+                mouse_queue.put(msg)
             except:
-                print("indexing error")
+                pass
 
     else:
-        time.sleep(0.1)
+        time.sleep(0.2)
 
 webserver_model.join()
 socket_model.join()
